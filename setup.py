c132aff10d077aff87b46f89c03d2efd0c1baa58
Diego Eckhard <diegoeck@gmail.com>
2026-06-08 22:21:20 -0300

    Bump version to 1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     
 setup(
     name='pyvrft',
-    version='1.1',
+    version='1.2',
     description='Virtual Reference Feedback Tuning',
     long_description=long_description,
     long_description_content_type='text/markdown',
